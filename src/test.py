f67da839961c4fe6cc584e10a2875bd3e0a7875d
Brandon Barker <bbarker.py@protonmail.ch>
2024-02-13 22:13:40 -0700

    feat: test module implemented

--- src/test.py
+++ src/test.py
@@ -5,9 +5,8 @@ import urllib.request
 import shutil
 
 import numpy as np
-import matplotlib.pyplot as plt
 
-from sedov import Sedov, Family
+from sedov import Sedov
 
 
 def prepare_sedov_test(TMPDIR):
@@ -58,7 +57,9 @@ def compile_sedov3(TMPDIR):
   print(cmd)
   os.system(cmd)
 
+
 # End compile_sedov3
+
 
 def omega_from_j_gamma(n_per_j, j, gamma):
   """
@@ -67,30 +68,54 @@ def omega_from_j_gamma(n_per_j, j, gamma):
   We also force the grid to hav a singular case.
   """
 
-  w = np.linspace(0.0, j  - j/n_per_j, n_per_j)
-  if j != 1.0: # no singular case in planar
+  w = np.linspace(0.0, j - j / n_per_j, n_per_j)
+  if j != 1.0:  # no singular case in planar
     singular = (3.0 * j - 2.0 + gamma * (2.0 - j)) / (gamma + 1.0)
     w = np.insert(w, 0, singular)
 
   return w
 
+
 # End omega_from_j_gamma
+
 
 def compare_sedov(x, rho, p, x_sol, rho_sol, p_sol):
   """
-  Compare out solution to sedov3.
-  Interpolate their grid to ours, compute norm
+  Compare our solution to sedov3.
+  Interpolate our grid to theirs, compute L1 norm.
+  Note: we remove the innermost point from the solutions..
+  They can blow up as r -> 0 for some setups, and our grids differ.
+  So we set the inner parts of the grid to match.
   """
 
-  new_rho_sol = np.interp(x, x_sol, rho_sol)
-  new_p_sol = np.interp(x, x_sol, p_sol)
+  r_inner = 0.01  # enforce.
 
-  # L2 norm
-  err_rho = np.sum( np.power(new_rho_sol - rho, 1.0) ) / len(rho)
-  err_p = np.sum( np.power(new_p_sol - p, 1.0) ) / len(p)
+  inds_sedov3 = np.where(x_sol > r_inner)
+  inds_sistrum = np.where(x > r_inner)
+
+  x_sol = x_sol[inds_sedov3]
+  rho_sol = rho_sol[inds_sedov3]
+  p_sol = p_sol[inds_sedov3]
+  x = x[inds_sistrum]
+  rho = rho[inds_sistrum]
+  p = p[inds_sistrum]
+
+  rho_sol /= np.max(rho_sol)
+  p_sol /= np.max(p_sol)
+  rho /= np.max(rho)
+  p /= np.max(p)
+
+  new_rho = np.interp(x_sol, x, rho)
+  new_p = np.interp(x_sol, x, p)
+
+  # L1 norm
+  err_rho = np.sum(np.abs(rho_sol - new_rho)) / len(new_rho)
+  err_p = np.sum(np.abs(p_sol - new_p)) / len(new_p)
   return err_rho, err_p
 
+
 # End compare_sedov
+
 
 def test_sedov():
   """
@@ -112,12 +137,12 @@ def test_sedov():
   npoints_sedov3 = 1000
   gamma = 1.4
   geometry = np.array([1.0, 2.0, 3.0])
-  r_model = 1.2 # same as sedov3
+  r_model = 1.2  # same as sedov3
   t = 1.0
-  outfile = "tmp_sedov.dat" # for sedov3 data
-  n_per_j = 8 # w values per geometry value
-  rho0 = 1.0 # ambient density
-  p0 = 1.0e-5 # ambient pressure
+  outfile = "tmp_sedov.dat"  # for sedov3 data
+  n_per_j = 8  # w values per geometry value
+  rho0 = 1.0  # ambient density
+  p0 = 1.0e-5  # ambient pressure
 
   for j in geometry:
     omega = omega_from_j_gamma(n_per_j, j, gamma)
@@ -128,20 +153,33 @@ def test_sedov():
       # run and load sedov3
       source_dir = os.path.join(TMPDIR, "sedov")
       source_ex = os.path.join(source_dir, "sedov3_qp")
-      sedov3_cmd = f"./{source_ex} {npoints_sedov3} {E_blast} {j} {w} {gamma} {outfile}"
+      sedov3_cmd = (
+        f"./{source_ex} {npoints_sedov3} {E_blast} {j} {w} {gamma} {outfile}"
+      )
       print(sedov3_cmd)
       os.system(sedov3_cmd)
       cmd = f"mv {outfile} {source_dir}"
       print(cmd)
       os.system(cmd)
-      x_sol, rho_sol, p_sol = np.loadtxt(os.path.join(source_dir, outfile), skiprows=2, unpack=True, usecols=(1,2,4))
-      err_rho, err_p = compare_sedov(sedov.r, sedov.rho_sol, sedov.p_sol, x_sol, rho_sol, p_sol)
-      print(f"{err_rho}, {err_p}")
+      x_sol, rho_sol, p_sol = np.loadtxt(
+        os.path.join(source_dir, outfile),
+        skiprows=2,
+        unpack=True,
+        usecols=(1, 2, 4),
+      )
+      err_rho, err_p = compare_sedov(
+        sedov.r, sedov.rho_sol, sedov.p_sol, x_sol, rho_sol, p_sol
+      )
+      # Yes, the threshhold for density error is high...
+      # The offending model keeping it there looks fine, though...
+      tol_rho = 0.05
+      tol_p = 0.005
+      assert err_rho < tol_rho, f"Density L1 must be below {tol_rho}"
+      assert err_p < tol_p, f"Pressure L1 must be below {tol_p}"
 
-  #shutil.rmtree(TMPDIR)
+  shutil.rmtree(TMPDIR)
 
 
 if __name__ == "__main__":
-  # example:
-
+  # main
   test_sedov()
